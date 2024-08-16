e4c608de3a33bc377b5be05849aca8bc59a93454
Mbah Neilvani <neilvanimbah@gmail.com>
2024-08-16 21:13:27 +0100

    wilson_prime

---
+++ Js/Wilson.js
@@ -0,0 +1,22 @@
+// Wilson primes satisfy the following condition. Let P represent a prime number.
+
+// Then,
+
+// (P − 1)! + 1
+// divided by
+// P ∗ P
+ 
+// should give a whole number, where P! is the factorial of P.
+
+// Your task is to create a function that returns true if the given number is a Wilson prime and false otherwise.
+
+let wilsonPrimeNumber(p){
+    if(a === p){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+// Solution
+
